3b6fa33d3c350f2ef3f17d834adcb90e2ae79249
herifanantenana <herifanantenana17>
2026-03-10 18:37:23 +0300

    feat(auth): include expiration time in access token payload

--- src/modules/auth/infra/adapters/tokenizer.jwt.adapter.ts
+++ src/modules/auth/infra/adapters/tokenizer.jwt.adapter.ts
@@ -28,6 +28,7 @@ export class TokenizerJwtAdapter implements TokenizerPort {
 			acid: accountId,
 			atid: actorId,
 			sid: sessionId,
+			expAtSec: expiresAt.getTime() / 1000,
 		});
 		return { value: token, expiresAt };
 	}
